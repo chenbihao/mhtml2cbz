c45347bb43968d90f82a83e2dd33c424c5448ed6
陈壁浩 <chenbihao@foxmail.com>
2026-05-05 19:49:06 +0800

    2.0.2

--- dist/index.js
+++ dist/index.js
@@ -56,7 +56,7 @@ const program = new Command();
 program
     .name("mhtml2cbz")
     .description("将 MHTML 文件转换为 CBZ 漫画档案格式")
-    .version("2.0.1", "-v, --version")
+    .version("2.0.2", "-v, --version")
     .argument("[input...]", "MHTML 文件路径，默认当前目录 *.mhtml")
     .requiredOption("-o, --output <dir>", "输出目录")
     .option("-m, --move <dir>", "转换成功后将原 MHTML 文件移动到指定目录")
